aa5673aad3c0c505d74ece337de51d35dca9f8fc
Hermes Zhang <chenhuiz@axis.com>
2021-05-24 16:16:02 +0800

    Rewrite the spawn for hunters
    Change-Id: I2cbde3d9810219193bec3874d0245e0e1cd24292

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use bevy_rapier2d::rapier::{
     dynamics::{RigidBodyBuilder, RigidBodySet},
     geometry::{ColliderBuilder, ColliderSet, ContactEvent},
 };
+use std::{borrow::Borrow, collections::HashMap};
 
 use rand::{thread_rng, Rng};
 
@@ -28,6 +29,7 @@ impl Default for EventTriggerState {
     }
 }
 
+#[derive(Debug)]
 struct Name(&'static str);
 
 struct Position {
@@ -37,8 +39,10 @@ struct Position {
 #[derive(Debug)]
 struct Speed(Vec2);
 
+#[derive(Debug)]
 struct Damage(u16);
 
+#[derive(Debug)]
 struct Helath(u16);
 
 struct Score(u16);
@@ -49,13 +53,54 @@ struct Monster;
 
 struct Hunter;
 
+fn spawn_hunter(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    let mut hunters = HashMap::new();
+    hunters.insert("Alice", "res/ferris-miner-min.png");
+    hunters.insert("Bob", "res/ferris-ninja-min.png");
+    hunters.insert("Chile", "res/ferris-viking-min.png");
+
+    for (name, logo) in hunters {
+        let crusta = asset_server.load(logo);
+
+        let mut h = commands.spawn_bundle(SpriteBundle {
+            transform: Transform {
+                scale: Vec3::splat(1.0 / 10.0),
+                ..Default::default()
+            },
+            material: materials.add(crusta.into()),
+            ..Default::default()
+        });
+
+        let body = RigidBodyBuilder::new_dynamic()
+            .translation(
+                rand::thread_rng().gen_range(-400.0..400.0),
+                rand::thread_rng().gen_range(-300.0..300.0),
+            )
+            .user_data(h.id().to_bits() as u128);
+
+        let collider = ColliderBuilder::ball(40.0);
+
+        h.insert_bundle((body, collider));
+        h.insert_bundle((
+            Hunter,
+            Name(name),
+            Speed(Vec2::new(0.0, 0.0)),
+            AI(1.0),
+            Damage(20),
+        ));
+    }
+}
+
 fn setup(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-
     /*
     commands.spawn_bundle((Hunter, Name("Alice"), Helath(1000)));
     commands.spawn_bundle((Hunter, Name("Bob"), Helath(1000)));
@@ -71,79 +116,31 @@ fn setup(
     commands.spawn_batch(players);
     */
 
-    let body = RigidBodyBuilder::new_dynamic().translation(20.0, 0.0).lock_rotations();
-    let collider = ColliderBuilder::ball(40.0);
-
     let crusta = asset_server.load("res/icon.png");
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.add(crusta.into()),
-            transform: Transform {
-                scale: Vec3::new(0.3, 0.3, 0.3),
-                ..Default::default()
-            },
+    let mut m = commands.spawn_bundle(SpriteBundle {
+        material: materials.add(crusta.into()),
+        transform: Transform {
+            scale: Vec3::new(0.3, 0.3, 0.3),
             ..Default::default()
-        })
-        //.insert(RigidBodyBuilder::new_dynamic())
-        //.insert(ColliderBuilder::cuboid(1.0, 1.0))
-        .insert_bundle((body, collider))
-        .insert(Speed(Vec2::new(1.0, 1.0)))
+        },
+        ..Default::default()
+    });
+
+    let body = RigidBodyBuilder::new_dynamic()
+        .translation(0.0, 0.0)
+        .lock_rotations()
+        .user_data(m.id().to_bits() as u128);
+
+    let collider = ColliderBuilder::ball(40.0);
+    //.insert(RigidBodyBuilder::new_dynamic())
+    //.insert(ColliderBuilder::cuboid(1.0, 1.0))
+    m.insert_bundle((body, collider))
+        .insert(Speed(Vec2::new(0.0, 0.0)))
+        .insert(Helath(10000))
+        .insert(Name("Bevy"))
         .insert(Monster);
 
-
-    let body = RigidBodyBuilder::new_dynamic().translation(-450.0, -200.0);
-    let collider = ColliderBuilder::ball(20.0);
-
-    let crusta2 = asset_server.load("res/ferris-ninja-min.png");
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.add(crusta2.into()),
-            transform: Transform {
-                scale: Vec3::new(0.1, 0.1, 0.1),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert_bundle((body, collider))
-        .insert(Speed(Vec2::new(1.0, 1.0)))
-        .insert(AI(1.0))
-        .insert(Hunter);
-
-    let body = RigidBodyBuilder::new_dynamic().translation(450.0, -250.0);
-    let collider = ColliderBuilder::ball(20.0);
-
-    let crusta = asset_server.load("res/ferris-viking-min.png");
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.add(crusta.into()),
-            transform: Transform {
-                scale: Vec3::new(0.1, 0.1, 0.1),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert_bundle((body, collider))
-        .insert(Speed(Vec2::new(1.0, 1.0)))
-        .insert(AI(0.1))
-        .insert(Hunter);
-
-    let body = RigidBodyBuilder::new_dynamic().translation(0.0, -250.0);
-    let collider = ColliderBuilder::ball(20.0);
-
-    let crusta = asset_server.load("res/ferris-miner-min.png");
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.add(crusta.into()),
-            transform: Transform {
-                scale: Vec3::new(0.1, 0.1, 0.1),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert_bundle((body, collider))
-        .insert(Speed(Vec2::new(1.0, 1.0)))
-        .insert(AI(0.1))
-        .insert(Hunter);
+    spawn_hunter(commands, materials, asset_server);
 }
 
 //fn movement_system(time: Res<Time>, mut query: Query<(&Monster, &Speed, &mut Transform)>) {
@@ -215,9 +212,9 @@ fn ai_system(
     mut hunter_query: Query<(&Hunter, &mut Speed, &Transform, &AI), Without<Monster>>,
 ) {
     if let Ok((_m, mut m_speed, m_trans)) = monster_query.single_mut() {
-        println!("In AI we see monster in: {}", m_trans.translation);
+        //println!("In AI we see monster in: {}", m_trans.translation);
 
-        m_speed.0 = 10.0
+        m_speed.0 = 20.0
             * Vec2::new(
                 thread_rng().gen_range(-1.0..1.0),
                 thread_rng().gen_range(-1.0..1.0),
@@ -225,24 +222,25 @@ fn ai_system(
             .normalize();
 
         for (_h, mut speed, h_trans, ai) in hunter_query.iter_mut() {
-            println!("Hunter is in {}", h_trans.translation);
+            //println!("Hunter is in {}", h_trans.translation);
 
             let mut v: Vec3 = (m_trans.translation - h_trans.translation).normalize();
 
             v.x += thread_rng().gen_range(-ai.0..ai.0);
             v.y += thread_rng().gen_range(-ai.0..ai.0);
 
-            println!("AI v: {}", v);
-            speed.0 = 20.0 * v.xy().normalize();
+            //println!("AI v: {}", v);
+            speed.0 = 40.0 * v.xy().normalize();
         }
     }
 }
 
 fn check_collision_events(
-    //mut commands: Commands,
+    mut commands: Commands,
     events: Res<EventQueue>,
     mut bodies: ResMut<RigidBodySet>,
     mut colliders: ResMut<ColliderSet>,
+    query: Query<(&Name)>,
 ) {
     while let Ok(contact_event) = events.contact_events.pop() {
         match contact_event {
@@ -250,16 +248,41 @@ fn check_collision_events(
                 let collider0 = colliders.get(c0).unwrap();
                 let collider1 = colliders.get(c1).unwrap();
 
-                //let b1 = bodies.get(collider1.parent()).unwrap();
-                let b1 = bodies.get_mut(collider1.parent()).unwrap();
+                let b0 = bodies.get(collider0.parent()).unwrap();
+                let b1 = bodies.get(collider1.parent()).unwrap();
+                //let b1 = bodies.get_mut(collider1.parent()).unwrap();
                 //let b0 = bodies.get(collider0.parent()).unwrap();
 
+                println!("{} {}", b1.is_ccd_enabled(), b1.is_ccd_active());
+
+                //b1.enable_ccd(true);
+
+                let e0 = Entity::from_bits(b0.user_data as u64);
+                let e1 = Entity::from_bits(b1.user_data as u64);
+
+                //println!("{:?}", query.get_component::<Name>(e0).unwrap());
+                //println!("{:?}", query.get_component::<Name>(e1).unwrap());
+
+                //let a = commands.entity(e1).get_mut::<Helath>().unwrap();
+                if let Ok(a) = query.get_component::<Name>(e0) {
+                    println!("{:?}", a);
+                    if let Ok(n) = query.get_component::<Name>(e1) {
+                        println!("{} hit monst with", n.0,);
+                    }
+                }
+
+                println!("-----------------------------------------------")
+                //commands.entity(e1).despawn();
+                //commands.entity(e2).despawn();
+
+                /*
                 //b0.apply_force(b1.position().translation, false);
                 //b1.apply_force(b0.position().translation, false);
                 b1.apply_force(Vector2::new(-1000.0, -1000.0), false);
 
                 //println!("{:?}", b0.position().translation);
                 println!("{:?}", b1.position().translation);
+                */
             }
             ContactEvent::Stopped(_, _) => {}
         }
